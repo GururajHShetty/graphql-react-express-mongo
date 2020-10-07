26fab52ed3c3e43226f2ae2bfa5f7b7ce6c13151
H Gururaj (IFIN DES TCP SW DEV / EE) <Gururaj.H-EE@infineon.com>
2020-10-07 12:45:08 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// components
+import BookList from "./components/BookList";
+import AddBook from "./components/AddBook";
+
+import { ApolloProvider } from "react-apollo";
+
+// apollo client set-up
+import ApolloClient from "apollo-boost";
+
+const client = new ApolloClient({
+  uri: "http://localhost:2020/graphql"
+});
+
+class App extends Component {
+  render() {
+    return (
+      <ApolloProvider client={client}>
+        <div id="main">
+          <h1>Ninja's Reading List</h1>
+          <BookList />
+          <AddBook />
+        </div>
+      </ApolloProvider>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/AddBook.js
@@ -0,0 +1,77 @@
+import React, { Component } from "react";
+import { graphql } from "react-apollo";
+import {
+  getAuthorsQuery,
+  addBookMutation,
+  getBooksQuery
+} from "../queries/queries";
+/** as react-apollo removed 'compose', I'm using alternative lodash.flowright */
+import * as compose from "lodash.flowright";
+
+class AddBook extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: "",
+      genre: "",
+      authorId: ""
+    };
+  }
+  displayAuthors() {
+    var data = this.props.getAuthorsQuery;
+    if (data.loading) {
+      return <option disabled>Loading authors</option>;
+    } else {
+      return data.authors.map(author => {
+        return (
+          <option key={author.id} value={author.id}>
+            {author.name}
+          </option>
+        );
+      });
+    }
+  }
+  render() {
+    return (
+      <form id="add-book" onSubmit={this.handleSubmit}>
+        <div className="field">
+          <label>Book name:</label>
+          <input type="text" name="name" onChange={this.onChange} />
+        </div>
+        <div className="field">
+          <label>Genre:</label>
+          <input type="text" name="genre" onChange={this.onChange} />
+        </div>
+        <div className="field">
+          <label>Author:</label>
+          <select name="authorId" onChange={this.onChange}>
+            <option>Select author</option>
+            {this.displayAuthors()}
+          </select>
+        </div>
+        <button>+</button>
+      </form>
+    );
+  }
+  onChange = e => {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  };
+  handleSubmit = e => {
+    e.preventDefault();
+    this.props.addBookMutation({
+      variables: {
+        name: this.state.name,
+        genre: this.state.genre,
+        authorId: this.state.authorId
+      },
+      refetchQueries: [{ query: getBooksQuery }]
+    });
+  };
+}
+
+export default compose(
+  graphql(getAuthorsQuery, { name: "getAuthorsQuery" }),
+  graphql(addBookMutation, { name: "addBookMutation" })
+)(AddBook);
